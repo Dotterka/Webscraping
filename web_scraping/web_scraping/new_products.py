9c0f415cf36b9b2dfa6fa7bdd3fa533eb1eed52a
Lorincz Dorottya <dotterka@yahoo.com>
2021-04-20 13:43:45 +0300

    data formatting scripts and database upload scripts added

---
+++ web_scraping/web_scraping/new_products.py
@@ -0,0 +1,20 @@
+# for updateing the products collection, if there are any new items
+
+import pandas as pd, numpy as np, json
+
+# new_items.json is from the update_products.py file, it contains the new products
+df_new_items=pd.read_json('new_items.json',lines=True)
+
+# emag_revisit_items.json contains the new products and it's specifications (it's from the emag_revisit spider)
+df_emag_revisit=pd.read_json('emag_revisit_items.json',lines=True)
+
+# update the dataframe, to store the specifications
+df_emag_revisit=df_emag_revisit.rename(columns={'price':'emag_price','url':'emag_url','review_score':'emag_review_score','review_count':'emag_review_count','provider_name':'emag'})
+df_new_items.update(df_emag_revisit)
+
+# keep only the items where the display_size column is not null
+# if the display_size column is empty, than the emag_revisit_items.json doesn't have that product
+# (because of an attribute of the emag_revisit spider), so all the specificcations are empty 
+df_new_items = df_new_items[df_new_items['display_size'].notna()]
+df_new_items = df_new_items.fillna(value=np.nan)
+df_new_items.to_json('products.json', orient='records', lines=True)
