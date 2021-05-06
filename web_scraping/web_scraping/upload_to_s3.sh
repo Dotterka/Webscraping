df42401a09dd44423a235eb988f41d5379018c42
Lorincz Dorottya <dotterka@yahoo.com>
2021-05-07 01:16:43 +0300

    troubleshooting the unique id generation and modifying the uploade script

--- web_scraping/web_scraping/upload_to_s3.sh
+++ web_scraping/web_scraping/upload_to_s3.sh
@@ -1,1 +1,1 @@
-aws s3 cp ../../../webshops_backup/webshops/. s3://allamvizsga/db_backup --recursive
+aws s3 cp ../../../webshops_backup/webshops/. s3://allamvizsga/webshops/$(date '+%d_%m_%Y') --recursive
